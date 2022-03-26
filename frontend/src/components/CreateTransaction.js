9862a04af2cde4423574212f15fbecbae21dc633
Rami <ramirofontalva@gmail.com>
2022-03-26 20:21:33 -0300

    Removed api keys. Not needed to be public anymore

---
+++ frontend/src/components/CreateTransaction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const CreateTransaction = () => (
+  //   const [inputs, setInputs] = React.useState({});
+  <div className="module-box" id="create-transaction">
+    <Form>
+      <Form.Label>
+        <Form.Control />
+      </Form.Label>
+    </Form>
+  </div>
+);
+export default CreateTransaction;
